1cbd539fa74a8e00be361631d1c65dd659a32016
JoarDev <j.ramos5452@gmail.com>
2022-08-26 21:30:43 -0300

    feat: submit from data

--- main.js
+++ main.js
@@ -12,3 +12,49 @@ $(document).ready(function(){
 	});
 
 });
+
+function onSubmitForm (event) {
+    event.preventDefault();
+
+    const formData = JSON.stringify({
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        phone: document.getElementById('phone').value
+    })
+
+    const sendFormDataRequest = new Request("https://jsonplaceholder.typicode.com/posts", {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+
+    fetch(sendFormDataRequest)
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json);
+            Swal.fire(
+                'Enviado correctamente',
+                'Nos pondremos en contacto contigo',
+                'success'
+            );
+            cleanForm();
+        })
+        .catch((err) => {
+            Swal.fire(
+                'Error',
+                'Intente nuevamente mas tarde',
+                'error'
+            );
+            console.log(err)
+        });
+}
+
+function cleanForm() {
+    const form = document.getElementById("form");
+    form.reset();
+}
+
+const submitFormButton = document.getElementById("submitFormButton")
+submitFormButton.addEventListener("click", onSubmitForm)
